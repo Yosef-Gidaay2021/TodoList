18553695b39ea6609ea2bcb1e92feb830a73e4d3
Yosef Giday <A00288051@mycambrian.ca>
2024-09-28 01:09:32 +0000

    class ToDoItem defined and implemented

---
+++ ToDoListAPI/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+
+public class ToDoItem{
+
+    public long Id { get; set; }
+    public string? Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? CompletedDate { get; set; }
+
+}
